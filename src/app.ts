24d9773372b239d98a3e62324bd9f9ff8c2fd580
Guilherme Alles <guilherme.r.alles@gmail.com>
2020-11-29 16:56:11 -0300

    move parameters to constants

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,25 @@ import { PublisherMessageProcessor } from "./application/PublisherMessageProcess
 import { TimestamperMessageProcessor } from "./application/TimestamperMessageProcessor";
 import { amqpClient } from "./context";
 
+const SOURCE_QUEUE = "test.queue.1";
+const TARGET_EXCHANGE = "exchange";
+const TARGET_ROUTING_KEY = "target.rk";
+
 const MESSAGING_PIPELINE = new MessagingPipeline([
   new TimestamperMessageProcessor(),
 
   new FilterMessageProcessor(() => false),
 
-  new PublisherMessageProcessor(amqpClient, "", "alles.test.1"),
+  new PublisherMessageProcessor(
+    amqpClient,
+    TARGET_EXCHANGE,
+    TARGET_ROUTING_KEY
+  ),
 ]);
 
 export async function launch(): Promise<void> {
   for (let i = 0; i < 100; i++) {
-    const message = await amqpClient.consumeMessage("alles.test.1");
+    const message = await amqpClient.consumeMessage(SOURCE_QUEUE);
     console.log(message);
     await MESSAGING_PIPELINE.run(message);
   }

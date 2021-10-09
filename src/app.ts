ca8e939b4e96f2582346e3360f952e3c57d22fc5
Guilherme Alles <guilherme@alles.dev.br>
2021-10-09 12:34:42 -0300

    Testing new pipeline structure

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-import { FilterMessageProcessor } from './application/FilterMessageProcessor';
 import { MessagingPipeline } from './application/MessagingPipeline';
 import { PublisherMessageProcessor } from './application/PublisherMessageProcessor';
 import { TimestamperMessageProcessor } from './application/TimestamperMessageProcessor';
@@ -6,30 +5,30 @@ import { amqpClient } from './context';
 import { initializeContext } from './context';
 import config from './config/config.json';
 
-const SOURCE_QUEUE = 'test.queue.1';
-const TARGET_EXCHANGE = 'exchange';
-const TARGET_ROUTING_KEY = 'target.rk';
-
-const MESSAGING_PIPELINE = new MessagingPipeline([
-  new TimestamperMessageProcessor(),
-
-  new FilterMessageProcessor(() => false),
-
-  new PublisherMessageProcessor(
-    amqpClient,
-    TARGET_EXCHANGE,
-    TARGET_ROUTING_KEY,
-  ),
-]);
+const TARGET_EXCHANGE = '';
+const TARGET_ROUTING_KEY = 'queue-2';
 
 async function launch(): Promise<void> {
+  const MESSAGING_PIPELINE = new MessagingPipeline([
+    new TimestamperMessageProcessor(),
+
+    new PublisherMessageProcessor(
+      amqpClient,
+      TARGET_EXCHANGE,
+      TARGET_ROUTING_KEY,
+    ),
+  ]);
+
   for (let i = 0; i < 100; i++) {
-    const message = await amqpClient.consumeMessage(SOURCE_QUEUE);
-    console.log(message);
+    const message = await amqpClient.consumeMessage('queue-1');
     await MESSAGING_PIPELINE.run(message);
   }
 }
 
-initializeContext(config).then(async () => {
-  await launch();
-});
+initializeContext(config)
+  .then(async () => {
+    await launch();
+  })
+  .then(() => {
+    process.exit(0);
+  });
